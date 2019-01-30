0039974e6298068f73a53782e4d0ae5f438c603b
ranjumolshasha <47152028+ranjumolshasha@users.noreply.github.com>
2019-01-30 06:40:05 -0800

    Add files via upload

---
+++ odd.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+    int a;
+    if(a>0)
+    {
+        if(a%2==0)
+        {
+            printf("Odd");
+            }
+            else
+            {
+                printf("Even");
+                }
+        }
+        else
+        {
+            printf("Invalid");
+            }
+  getch();  
+    }
